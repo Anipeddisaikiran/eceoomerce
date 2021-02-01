63a39c18957cbb455c07cd0692ac84dc9694fde7
Anipeddisaikiran <78066538+Anipeddisaikiran@users.noreply.github.com>
2021-02-01 15:46:21 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch("../data/data.json").then(response=>{
+fetch("https://anipeddisaikiran.github.io/eceoomerce/data/data.json").then(response=>{
     return response.json()
 }).then(data=>{
     displayData(data)
